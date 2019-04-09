0640c435d7e0794d100001c0ab1a7c28f4cf46df
Dominick <dominickhemphill1@gmail.com>
2019-04-09 17:09:41 -0400

    updated  CSS & DAO's

--- final-capstone-project/src/main/java/com/techelevator/model/JDBCClassDAO.java
+++ final-capstone-project/src/main/java/com/techelevator/model/JDBCClassDAO.java
@@ -1,19 +1,54 @@
 package com.techelevator.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class JDBCClassDAO implements ClassDAO{
+import javax.sql.DataSource;
 
-	@Override
-	public void addClass() {
-		// TODO Auto-generated method stub
-		
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JDBCClassDAO implements ClassDAO{
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public JDBCClassDAO(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	@Override
-	public List<Class> viewClasses() {
-		// TODO Auto-generated method stub
-		return null;
+	public void addClass(Class newClass) {
+		String sqlToAddClass = "INSERT INTO class (id,name) VALUES (default, ?)";
+		jdbcTemplate.update(sqlToAddClass, newClass.getName());	
+	}
+
+	@Override
+	public List<Class> viewClasses(int instructorId) {
+		List<Class> classList =  new ArrayList<>();
+		String sqlSelectAllClasses = "SELECT * FROM class "
+									+ "JOIN instructor_class ON instructor_class.class_id"
+									+ "= class.class_id "
+									+ "JOIN instructor ON instructor.instructor_id = instructor_class.instructor_id"
+									+ "WHERE instructor_id = ? ORDER BY ASC";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSelectAllClasses, instructorId);
+		while(results.next()) {
+			Class c = mapRowToClass(results);
+			classList.add(c);
+		}
+		return classList;
+	}
+
+	private Class mapRowToClass(SqlRowSet results) {
+		
+		Class c = new Class();
+		c.setClassId(results.getInt("class_id"));
+		c.setName(results.getString("name"));
+		
+		return c;
 	}
 	
 }
