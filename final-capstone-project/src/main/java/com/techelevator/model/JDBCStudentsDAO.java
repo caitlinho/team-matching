a9fbe35cbdbaa9355054155540f2f710cbeb4764
Georgia <gasutter5@gmail.com>
2019-04-10 11:45:22 -0400

    edited JDBCStuentDAO

--- final-capstone-project/src/main/java/com/techelevator/model/JDBCStudentsDAO.java
+++ final-capstone-project/src/main/java/com/techelevator/model/JDBCStudentsDAO.java
@@ -1,14 +1,28 @@
 package com.techelevator.model;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+
 
 @Component
 public class JDBCStudentsDAO implements StudentsDAO{
+	
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public JDBCStudentsDAO (DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
 	@Override
 	public void addStudentList() {
-//		String sqlToAddStudentToList = ""
-		
+	String sqlToAddStudentsList = "COPY student (student_id, name, email, comments) FROM '/Users/gsutter/Development/capstone/final-capstone-team-bravo/studentList.csv' DELIMITERS ',' CSV header FORCE QUOTE *;\n"; 
+	jdbcTemplate.update(sqlToAddStudentsList);
+			
 	}
 
 	@Override
