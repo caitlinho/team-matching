7ad74b1be16d5cbf22a632b75d703d7bb5d42fef
Georgia <gasutter5@gmail.com>
2019-04-11 11:15:04 -0400

    instructor user afrank

--- final-capstone-project/database/data.sql
+++ final-capstone-project/database/data.sql
@@ -8,7 +8,7 @@ BEGIN;
 -- the password for this account is !CodingRules1
 INSERT INTO app_user (id, user_name, password, role, salt) VALUES (DEFAULT, 'INSTRUCTORA', '26uvymhdPWOX5iz/Du5Z8A==', 'instructor','8SgDncWoiH4SNtD6yJ7BtaPTho5pUEGSfRcY+cH4LtF8cCOj9n8H80emmRXyLz1FaxOL5vi4RbQ8xT92SNn29/IHA8ziq1IQ6eLd0wl0LL3ibxhxcTLSBRCRQ1LHVcO+temOe3NhNvPzlah9vEk35o3Dq7oqwiO2iIKtJcM36d0=');
 
-INSERT INTO app_user(id, user_name, password, role) VALUES (DEFAULT, 'afrank', '!FrontEndRules1', 'instructor');
-
-INSERT INTO app_user(id, user_name, password, role) VALUES (DEFAULT, 'klindsey', '1CSSandDBmaster!', 'administrator')
+INSERT INTO app_user(id, user_name, password, role, salt) VALUES (DEFAULT, 'afrank', 'H70FZSgB+SZKZF9cKqsPDA==', 'instructor', 'FHiL7k+2CU/pZkQh6KZBEUhnU3eHjpDogGNWweX71cwqaOls7SFp2vp1AbJsaGDXnxhoGTgmEd70QMi1jOFVdh87NTV1yxqyLgohjmmvKczgNLIJduzFU34zk8AwOq3hHqt0euk/kVbJlFqMmhAamUYf+fljUqRqRynDznqsLTI=');
+--
+--INSERT INTO app_user(id, user_name, password, role) VALUES (DEFAULT, 'klindsey', '1CSSandDBmaster!', 'administrator')
 COMMIT;
